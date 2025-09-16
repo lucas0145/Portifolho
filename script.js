594a4e1bf59b7b2b064d2b36ce6a57edb6666042
Lucas Balmant <91352327+lucas0145@users.noreply.github.com>
2025-09-16 08:51:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-let cont = -1
 
 function skillFnc(a) {
+    
+    let obj = document.getElementById("fncObj")
 
-    // cont += 1
-    // if (cont % 2 == 0){
-    //     a.style.transform = "translate(-50%, -50%)"
-    //     a.style.position = "absolute"
-    //     a.style.left =  "50%"
-    //     a.style.top = "50%"
-    //     a.style.transition = "transform 0s"
-    //     a.style.zIndex = "3"
-    // }else{
-    //     a.style.transform = ""
-    //     a.style.position = "static"
-    //     a.style.left =  "50%"
-    //     a.style.top = "50%"
-    // }
+ 
+    obj.classList.toggle("sizeTgg")
+
+}
+
+function skillOut(obj){
+
+  
+    obj.classList.toggle("sizeTgg")
 
 }

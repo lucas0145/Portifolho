410eb51360f2f04fe5effe9ed912945f9a4bac91
Lucas Balmant <91352327+lucas0145@users.noreply.github.com>
2025-09-18 17:30:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,24 @@ function skillOut(obj){
   
     obj.classList.toggle("sizeTgg")
 
+
 }
+
+// let obj = document.getElementById("aa")
+
+// obj.addEventListener("mouseover", function(){
+  
+//   obj.style.width = "100vw"
+//   setTimeout(() => {
+//     obj.style.height = "100vw"
+//   }, "1000");
+
+// });
+
+// obj.addEventListener("mouseout", function(){
+//   obj.style.width = "20vw"
+
+//   setTimeout(() => {
+//    obj.style.height = "20vw"
+//   }, "1000");
+// });
